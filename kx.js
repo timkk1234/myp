c9ef6f9fabd66b78affa574b93f45c81f88d9fc5
timkk1234 <67384964+timkk1234@users.noreply.github.com>
2026-02-11 22:10:57 +0800

    Update kx.js

--- kx.js
+++ kx.js
@@ -1,6 +1,7 @@
 let url = $request.url;
 
 // 只将 ecpm= 后面的数值改为 35000.00
-let newUrl = url.replace(/(ecpm=)[^&]+/, '$135000.00');
+let newUrl = url.replace(/(ecpm=)[^&]+/, '$125000.00');
+
 
 $done({ url: newUrl });
